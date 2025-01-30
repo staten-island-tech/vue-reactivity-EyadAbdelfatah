c7a59e160cf4d001c43137e64fb2098255b4239d
mellowed24 <eyada5@nycstudents.net>
2025-01-30 17:30:22 -0500

    grnfek

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </div>
 </template>
- 
+
 <style>
 @media (min-width: 1024px) {
   .about {
