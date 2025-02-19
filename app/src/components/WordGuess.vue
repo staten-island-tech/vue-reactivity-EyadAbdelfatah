1bc583a742e924a4cf35f9aa663f92536b0568b3
mellowed24 <eyada5@nycstudents.net>
2025-02-19 15:49:54 -0500

    n

--- app/src/components/WordGuess.vue
+++ app/src/components/WordGuess.vue
@@ -7,18 +7,12 @@
     </div>
     
     <div class="grid grid-cols-7 gap-4">
-    <button class="btn btn-default">Animals</button>
-    <button class="btn btn-default">Fruits</button>
-    <button class="btn btn-default">Countries</button>
-    <button class="btn btn-default">Sports</button>
-    <button class="btn btn-default">Colors</button>
-    <button class="btn btn-default">Occupations</button>
-    <button class="btn btn-default">Technology</button>
+
     </div>
     
-    <div class="flex justify-center">
-      <h2 class="text-xl">scrambled Word: {{ scrambled }}</h2>
-    <h3>Win Counter: {{ win_counter }}</h3>
+    <div class="grid grid-rows-2 justify-center">
+      <h2 class="text-xl">scrambled Word: {{ scrambled }}</h2> 
+      <h3>Win Counter: {{ win_counter }}</h3>
     <WordScramble />
     </div>
     
--- app/src/components/WordScramble.vue
+++ app/src/components/WordScramble.vue
@@ -7,46 +7,23 @@
 <script setup>
 import { ref, reactive } from 'vue'
 import WordGuess from './WordGuess.vue'
+
 const words = reactive([
-  'airplane',
-  'balloon',
-  'banana',
-  'candle',
-  'cherry',
-  'date',
-  'dinosaur',
-  'elephant',
-  'flame',
-  'forest',
-  'grape',
-  'guitar',
-  'harp',
-  'honey',
-  'igloo',
-  'jungle',
-  'kiwi',
-  'lemon',
-  'monkey',
-  'notebook',
-  'orange',
-  'piano',
-  'quilt',
-  'rose',
-  'sunflower',
-  'tariffs',
-  'tree',
-  'trump',
-  'umbrella',
-  'violet',
-  'watermelon',
-  'whalen',
-  'xylophone',
-  'yarn',
-  'zebra',
-])
+  "brick", "frost", "blunt", "grasp", "cling", "drift", "plumb", "swift", "charm", "trunk",
+  "brisk", "flint", "crisp", "glint", "stomp", "clamp", "prong", "twist", "clump", "brunt",
+  "thump", "crank", "stint", "whisk", "plank", "stark", "crush", "grump", "plush", "draft",
+  "throb", "flask", "stung", "grunt", "blitz", "swamp", "twang", "scalp", "whiff", "crush",
+  "brass", "swoop", "quilt", "prism", "drank", "scorn", "tramp", "spunk", "snarl", "flick",
+  "grind", "pluck", "throb", "crisp", "sworn", "broth", "stoke", "clash", "swank", "clout",
+  "whack", "brawn", "frisk", "chomp", "slink", "swish", "spool", "drown", "whisk", "splat",
+  "gruff", "tryst", "swoon", "chasm", "brute", "churn", "frank", "chomp", "sworn", "brash",
+  "glove", "thump", "graft", "drill", "slink", "clink", "prone", "brisk", "spine", "trill",
+  "plump", "frost", "flair", "stark", "broil", "skimp", "shorn", "whirl", "chunk", "grill"
+]);
 let word = ref('')
 let correctWord = ref('')
 function scrambleWords() {
+  
   const num = Math.floor(Math.random() * words.length)
 
   correctWord = words[num]
@@ -55,7 +32,14 @@ function scrambleWords() {
     .split('')
     .sort(() => 0.5 - Math.random())
     .join('')
+  while (correctWord == word.value){
+    word.value = words[num]
+    .split('')
+    .sort(() => 0.5 - Math.random())
+    .join('')
+  }
 
+  words.splice(num,1)
 }
 scrambleWords()
 </script>
